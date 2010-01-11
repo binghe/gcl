01bc0ef39154231ea0b9f21cbe5e56067f87bc1a
Camm Maguire <camm@debian.org>
2010-01-11 18:59:43 +0000

    SA_SIGINFO for 386-linux

--- gcl/h/386-linux.h
+++ gcl/h/386-linux.h
@@ -28,7 +28,10 @@
 #endif     
 #endif
 
-#define GET_FAULT_ADDR(sig,code,sv,a) ((void *)(((struct sigcontext *)(&code))->cr2))
+#undef MPROTECT_ACTION_FLAGS
+#define MPROTECT_ACTION_FLAGS SA_RESTART|SA_SIGINFO
+#define GET_FAULT_ADDR(sig,code,sv,a) ((siginfo_t *)code)->si_addr
+/* #define GET_FAULT_ADDR(sig,code,sv,a) ((void *)(((struct sigcontext *)(&code))->cr2)) */
 #endif
 
 /*#define NULL_OR_ON_C_STACK(x) ((x)==0 || ((unsigned int)x) > (unsigned int)(pagetochar(MAXPAGE+1)))*/
