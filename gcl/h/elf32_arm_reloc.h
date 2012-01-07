0f33609603aa182a85e11da0fcc5b665f45a1140
Camm Maguire <camm@debian.org>
2012-01-07 02:40:40 +0000

    remove gprof on arm as mcount calls are 24/22bit -- marginally accessible

--- gcl/h/elf32_arm_reloc.h
+++ gcl/h/elf32_arm_reloc.h
@@ -1,7 +1,22 @@
+/* #define R_ARM_THM_CALL        10 */
 #define R_ARM_CALL 28
 #define R_ARM_V4BX 40
 #define R_ARM_THM_MOVW_ABS_NC 47
 #define R_ARM_THM_MOVW_ABS    48
+    /* case R_ARM_THM_CALL: */
+    /*   s+=a; */
+    /*   if (ELF_ST_TYPE(sym->st_info)==STT_FUNC) s|=1; */
+    /*   s-=p; */
+    /*   s-=4;  */
+    /*   s>>=1; */
+    /*   s&=(~1); */
+    /*   store_val(where,MASK(1)<<28,0); */
+    /*   /\* massert(!(s&0xffc00000)); *\/ */
+    /*   /\* s=((s&0x7ff)<<16)|((s>>11)&0x7ff); *\/ */
+    /*   store_val(where,MASK(12),s>>12); */
+    /*   store_val(where,MASK(12)<<16,(s&0xfff)<<16); */
+    /*   /\* add_vals(where,~0L,s); *\/ */
+    /*   break; */
     case R_ARM_THM_MOVW_ABS_NC:
       s+=a;
       if (ELF_ST_TYPE(sym->st_info)==STT_FUNC) s|=1;
