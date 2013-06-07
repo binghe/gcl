a141f9c80d77c76b8e6b71c182dbab1ac0ffe1bc
Camm Maguire <camm@debian.org>
2013-06-07 20:59:13 +0000

    win port

--- gcl/h/att.h
+++ gcl/h/att.h
@@ -74,8 +74,8 @@
 
 #define INIT_ALLOC \
      	heap_end = sbrk(0); \
-	if ((i = ((int)heap_end & (PAGESIZE - 1)))) \
-		sbrk(PAGESIZE - i); \
+	({fixnum i;if ((i = ((int)heap_end & (PAGESIZE - 1))))	\
+		     sbrk(PAGESIZE - i);})			\
 	heap_end = core_end = sbrk(0);
 
 	
