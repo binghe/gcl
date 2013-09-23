539886ec0ea557ae44e194a33808699eaf6909d8
Camm Maguire <camm@debian.org>
2013-09-23 19:25:40 +0000

    make sure contiguous sgc_minfree gets initialized with sane value

--- gcl/o/alloc.c
+++ gcl/o/alloc.c
@@ -1101,10 +1101,10 @@ gcl_init_alloc(void) {
   init_tm(t_afun, "AAFUN", sizeof(struct sfun), 0 ,1,0);
   init_tm(t_cfdata, "cCFDATA", sizeof(struct cfdata), 102 ,1,0);
   init_tm(t_spice, "!SPICE", sizeof(struct spice), 4096 ,1,0);
-  init_tm(t_relocatable, "%RELOCATABLE-BLOCKS", 1000,0,20,0);
-  init_tm(t_contiguous, "_CONTIGUOUS-BLOCKS", 1001,0,20,0);
   tm_table[t_relocatable].tm_nppage = PAGESIZE;
+  init_tm(t_relocatable, "%RELOCATABLE-BLOCKS", 1000,0,20,0);
   tm_table[t_contiguous].tm_nppage = PAGESIZE;
+  init_tm(t_contiguous, "_CONTIGUOUS-BLOCKS", 1001,0,20,0);
   
   
   ncb = 0;
