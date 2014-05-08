24cb2309e21cb8e35230c2b66b872be690cac3b6
Camm Maguire <camm@debian.org>
2014-05-08 18:09:09 +0000

    gnuwin95.h SF macro

--- gcl/h/gnuwin95.h
+++ gcl/h/gnuwin95.h
@@ -145,9 +145,10 @@ extern DBEGIN_TY _dbegin;
 
 /* End for cmpinclude */
 
+#define SF(a_) ((siginfo_t *)a_)
 
-#define FPE_CODE(i_,v_) make_fixnum((fixnum)i_->si_code)
-#define FPE_ADDR(i_,v_) make_fixnum((fixnum)i_->si_addr)
+#define FPE_CODE(i_,v_) make_fixnum((fixnum)SF(i_)->si_code)
+#define FPE_ADDR(i_,v_) make_fixnum((fixnum)SF(i_)->si_addr)
 #define FPE_CTXT(v_) Cnil
 
 #define FPE_INIT Cnil
