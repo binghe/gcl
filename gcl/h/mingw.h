0d3a30a4a4f50b7327fa8c8c260b02d3140f1851
Camm Maguire <camm@debian.org>
2014-05-08 17:58:58 +0000

    windows headers typos

--- gcl/h/mingw.h
+++ gcl/h/mingw.h
@@ -220,7 +220,7 @@ extern int mingwlisten(FILE *);
 
 #define NOFREE_ERR
 
-#define FPE_CODE(i_) make_fixnum(fSfpe_code(FFN(fSfnstsw)(),FFN(fSstmxcsr)()))
+#define FPE_CODE(i_,v_) make_fixnum(fSfpe_code(FFN(fSfnstsw)(),FFN(fSstmxcsr)()))
 #define FPE_ADDR(i_,v_) make_fixnum(0)
 #define FPE_CTXT(v_) Cnil
 
