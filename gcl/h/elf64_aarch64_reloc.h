59883d933aa4bbdf7ea58e7885a3a2c63f46c709
Camm Maguire <camm@debian.org>
2014-08-13 22:03:56 +0000

    aarch64 comment fix

--- gcl/h/elf64_aarch64_reloc.h
+++ gcl/h/elf64_aarch64_reloc.h
@@ -32,7 +32,7 @@
     case R_AARCH64_LDST8_ABS_LO12_NC: /* LD/ST8: (S+A) & 0xfff */
       store_val(where,MASK(12) << 10,((s+a) & 0xfff) << 10);
       break;
-    case R_AARCH64_LDST16_ABS_LO12_NC: /* LD/ST16: (S+A) & 0xffc */
+    case R_AARCH64_LDST16_ABS_LO12_NC: /* LD/ST16: (S+A) & 0xffe */
       store_val(where,MASK(12) << 10,((s+a) & 0xffe) << 9);
       break;
     case R_AARCH64_LDST32_ABS_LO12_NC: /* LD/ST32: (S+A) & 0xffc */
@@ -41,6 +41,6 @@
     case R_AARCH64_LDST64_ABS_LO12_NC: /* LD/ST64: (S+A) & 0xff8 */
       store_val(where,MASK(12) << 10,((s+a) & 0xff8) << 7);
       break;
-    case R_AARCH64_LDST128_ABS_LO12_NC: /* LD/ST128: (S+A) & 0xff8 */
+    case R_AARCH64_LDST128_ABS_LO12_NC: /* LD/ST128: (S+A) & 0xff0 */
       store_val(where,MASK(12) << 10,((s+a) & 0xff0) << 6);
       break;
