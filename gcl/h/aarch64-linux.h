1885327bf4a3c019b3ed22bed266aa527522bd39
Camm Maguire <camm@debian.org>
2014-08-14 18:08:25 +0000

    SGC and stack_guard for aarch64

--- gcl/h/aarch64-linux.h
+++ gcl/h/aarch64-linux.h
@@ -1,9 +1,10 @@
 #include "linux.h"
 
-//#define SGC
-//#define NEED_STACK_CHK_GUARD
 //C_GC_OFFSET
 //PTR_ALIGN
 
 #define RELOC_H "elf64_aarch64_reloc.h"
 #define SPECIAL_RELOC_H "elf64_aarch64_reloc_special.h"
+
+#define SGC
+#define NEED_STACK_CHK_GUARD
