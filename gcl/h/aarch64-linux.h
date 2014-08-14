9906233fcc78c587d7dc3ab99e06bc255979f560
Camm Maguire <camm@debian.org>
2014-08-14 19:13:09 +0000

    disable SGC on aarch64, erratic unreproducible uncontrolled segfaults

--- gcl/h/aarch64-linux.h
+++ gcl/h/aarch64-linux.h
@@ -9,7 +9,7 @@
 /*      ((void *)(*((char ***)(&code)))[44]) */
 #endif
 
-#define SGC
+//#define SGC  Not working as of 8/14/14 CM
 
 #define RELOC_H "elf64_aarch64_reloc.h"
 #define SPECIAL_RELOC_H "elf64_aarch64_reloc_special.h"
