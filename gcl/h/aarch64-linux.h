1c0407cf1a742c2d4000c1e6cb894de8a7747de6
Camm Maguire <camm@debian.org>
2014-08-19 18:56:28 +0000

    try SGC for aarch64

--- gcl/h/aarch64-linux.h
+++ gcl/h/aarch64-linux.h
@@ -9,9 +9,8 @@
 /*      ((void *)(*((char ***)(&code)))[44]) */
 #endif
 
-//#define SGC  Not working as of 8/14/14 CM
-
 #define RELOC_H "elf64_aarch64_reloc.h"
 #define SPECIAL_RELOC_H "elf64_aarch64_reloc_special.h"
 
 #define NEED_STACK_CHK_GUARD
+#define SGC
