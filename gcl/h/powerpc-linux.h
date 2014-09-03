369fd3cdff3a5bfdc2432ea3a98dad25345fc45c
Camm Maguire <camm@debian.org>
2014-09-03 11:29:32 -0400

    ppc64le support

--- gcl/h/powerpc-linux.h
+++ gcl/h/powerpc-linux.h
@@ -19,8 +19,13 @@
 #if SIZEOF_LONG == 4
 #define RELOC_H "elf32_ppc_reloc.h"
 #else
+#ifdef WORDS_BIGENDIAN
 #define RELOC_H "elf64_ppc_reloc.h"
 #define SPECIAL_RELOC_H "elf64_ppc_reloc_special.h"
+#else
+#define RELOC_H "elf64_ppcle_reloc.h"
+#define SPECIAL_RELOC_H "elf64_ppcle_reloc_special.h"
+#endif
 #define STATIC_FUNCTION_POINTERS
 #define C_GC_OFFSET 4
 #endif
