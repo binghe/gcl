72ea24d90ecca5d590f8f7d26e4f4c07e20a8a3b
Camm Maguire <camm@debian.org>
2015-04-27 16:19:14 -0400

    stray semicolon in gcl-tk

--- gcl/gcl-tk/comm.c
+++ gcl/gcl-tk/comm.c
@@ -183,7 +183,7 @@ int m;
     { bcopy(sfd->valid_data,sfd->read_buffer,sfd->valid_data_size);
       sfd->valid_data=sfd->read_buffer;}
    /* there is at least a packet size of space available */   
-  if ((fix(FFN(fScheck_fd_for_input)(sfd->fd,sfd->write_timeout))>0));
+  if ((fix(FFN(fScheck_fd_for_input)(sfd->fd,sfd->write_timeout))>0))
      again:
         {char *start = sfd->valid_data+sfd->valid_data_size;
         nread = SAFE_READ(sfd->fd,start,
