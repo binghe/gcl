2122b126442ac11380a2a4ab822097a87730292b
Camm Maguire <camm@debian.org>
2016-01-07 21:15:45 +0000

    bootstrap sequence change

--- gcl/unixport/sys_init.c
+++ gcl/unixport/sys_init.c
@@ -34,9 +34,9 @@ gcl_init_system(object no_init)
 
   check_init(lsp,gcl_s,no_init);
   check_init(lsp,gcl_sf,no_init);
+  check_init(lsp,gcl_rm,no_init);
   check_init(lsp,gcl_dl,no_init);
   check_init(lsp,gcl_fle,no_init);
-  check_init(lsp,gcl_rm,no_init);
   check_init(lsp,gcl_defmacro,no_init);
   check_init(lsp,gcl_evalmacros,no_init);
   check_init(lsp,gcl_module,no_init);
