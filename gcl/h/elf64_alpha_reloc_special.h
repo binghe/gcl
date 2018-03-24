fa1643b1006a3bec001da3c1fcfaa46f215da67d
Camm Maguire <camm@victorious.maguirefamily.org>
2018-03-24 16:32:36 +0000

    write_stub_mcount for alpha

--- gcl/h/elf64_alpha_reloc_special.h
+++ gcl/h/elf64_alpha_reloc_special.h
@@ -1,9 +1,31 @@
-static ul ggot1,ggote,gotoff;
+static ul ggot1,ggote,gotoff,mcount;
+
+static int
+write_stub_mcount(ul s,ul *gote) {
+
+  unsigned int *goti;
+
+  /*mcount calls written using at register, address not available in stub*/
+  /*mcount guaranteed to be within 32bits*/
+  *gote=(ul)(goti=(void *)(gote+1));
+  *goti++=(0x9<<26)|(0x1b<<21)|(0x1f<<16)|((s-(short)s)>>16); /*ldah	t12,(symhigh)(zero)*/
+  *goti++=(0x8<<26)|(0x1b<<21)|(0x1b<<16)|(s&MASK(16));       /*lda	t12,(symlow)(t12)*/
+  *goti++=(0x29<<26)|(0x1b<<21)|(0x1b<<16)|0;                 /*ldq	t12,0(t12)*/
+  *goti++=(0x1a<<26)|(0x1f<<21)|(0x1b<<16)|0x4000;            /*jsr	zero,(t12),$pc+4*/
+  *goti++=0;                                                  /*halt*/
+  *goti++=0;                                                  /*halt*/
+
+  return 0;
+
+}
 
 static int
 write_stub(ul s,ul *gote) {
 
   unsigned int *goti;
+
+  if (s==mcount)
+    return write_stub_mcount(mcount,gote);
 
   *gote=(ul)(goti=(void *)(gote+2));
   *++gote=s;
@@ -35,14 +57,18 @@ static int
 find_special_params(void *v,Shdr *sec1,Shdr *sece,const char *sn,
 		    const char *st1,Sym *ds1,Sym *dse,Sym *sym1,Sym *syme) {
 
+  Sym *sym;
   Shdr *sec;
   Rela *r;
-  void *ve;
+  void *ve,*dst1;
 
   massert((sec=get_section(".got",sec1,sece,sn)));
 
   ggot1=sec->sh_addr;
   ggote=ggot1+sec->sh_size;
+
+  massert(sec=get_section(".dynstr",sec1,sece,sn));/*FIXME pass as parameter*/
+  dst1=v+sec->sh_offset;
 
   massert((sec=get_section(".rel.dyn",sec1,sece,sn))||
 	  (sec=get_section(".rela.dyn",sec1,sece,sn)));
@@ -51,8 +77,11 @@ find_special_params(void *v,Shdr *sec1,Shdr *sece,const char *sn,
   ve=v+sec->sh_size;
 
   for (r=v;v<ve;v+=sec->sh_entsize,r=v) 
-    if (ELF_R_TYPE(r->r_info) && !ds1[ELF_R_SYM(r->r_info)].st_value)
-      ds1[ELF_R_SYM(r->r_info)].st_value=r->r_offset;
+    if (ELF_R_TYPE(r->r_info) && !(sym=ds1+ELF_R_SYM(r->r_info))->st_value) {
+      sym->st_value=r->r_offset;
+      if (!strncmp("_mcount",dst1+sym->st_name,7))
+	mcount=sym->st_value;
+    }
 
   return 0;
 
