955c7fa7fd061fcaaf9cb9d2426d455e1487079f
Camm Maguire <camm@victorious.maguirefamily.org>
2018-09-13 16:32:27 +0000

    R_X86_64_PLT32 support

--- gcl/h/elf64_i386_reloc.h
+++ gcl/h/elf64_i386_reloc.h
@@ -8,6 +8,7 @@
       add_val(where,~0L,s+a);
       break;
     case R_X86_64_PC32:
+    case R_X86_64_PLT32:
       massert(ovchks(s+a-p,~MASK(32)));
       add_val(where,MASK(32),s+a-p);
       break;
