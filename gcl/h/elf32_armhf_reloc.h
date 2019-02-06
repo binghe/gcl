6360e6cc8972e02af93ef516d3ab25f72bfac63c
Camm Maguire <camm@victorious.maguirefamily.org>
2019-02-06 02:12:06 +0000

    Remove one bit from JUMP24 and THM_CALL veneers

--- gcl/h/elf32_armhf_reloc.h
+++ gcl/h/elf32_armhf_reloc.h
@@ -4,7 +4,7 @@
      case R_ARM_THM_JUMP24:
       {
     	long x=(long)(s+a-p);
-    	if (abs(x)&(~MASK(24))) {
+    	if (abs(x)&(~MASK(23))) {
 
           got+=(sym->st_size-1)*tz;
     	  memcpy(got,tramp,sizeof(tramp));
@@ -32,7 +32,7 @@
     case R_ARM_THM_CALL:
       {
     	long x=(long)(s+a-p);
-    	if (abs(x)&(~MASK(23))) {
+    	if (abs(x)&(~MASK(22))) {
           got+=(sym->st_size-1)*tz;
     	  memcpy(got,tramp,sizeof(tramp));
 
