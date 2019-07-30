e80a270a0340095d8a2c6566da26bb726e629081
Camm Maguire <camm@victorious.maguirefamily.org>
2019-07-30 18:10:51 +0000

    dlsetjmp in cmpinclude.h

--- gcl/h/cmponly_last.h
+++ gcl/h/cmponly_last.h
@@ -2,6 +2,8 @@
 #ifndef __ia64__/*FIXME*/
 #undef setjmp
 #define setjmp ((int(*)(void *))dlsetjmp)
+#undef _setjmp
+#define _setjmp ((int(*)(void *))dlsetjmp)
 #endif
 #undef getc
 #define getc   ((int(*)(void *))dlgetc)
