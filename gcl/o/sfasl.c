ef9858accc7d53675424194cf42c561ca9934205
Camm Maguire <camm@victorious.maguirefamily.org>
2019-08-23 16:22:28 +0000

    find-sym-ptable from stable

--- gcl/o/sfasl.c
+++ gcl/o/sfasl.c
@@ -59,6 +59,21 @@ find_sym_ptable(const char *name) {
 
 }
 
+DEFUN("FIND-SYM-PTABLE",object,fSfind_sym_ptable,SI,1,1,NONE,IO,OO,OO,OO,(object x),"") {
+  char c;
+  struct node *a;
+
+  check_type_string(&x);
+
+  c=x->st.st_self[VLEN(x)];
+  x->st.st_self[VLEN(x)]=0;
+  a=find_sym_ptable(x->st.st_self);
+  x->st.st_self[VLEN(x)]=c;
+
+  return (object)(a ? a->address : 0);
+
+}
+
 #endif
 
 #ifdef SEPARATE_SFASL_FILE
