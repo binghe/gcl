d61d4aeee45106b2025671737fbcb915e6afc022
Camm Maguire <camm@transcendence.maguirefamily.org>
2022-07-31 11:58:56 -0400

    avoid brk probe on alpha for now, recently broken

--- gcl/h/alpha-linux.h
+++ gcl/h/alpha-linux.h
@@ -20,3 +20,5 @@
 #define imb() __asm__ __volatile__ ("call_pal %0 #imb" : : "i" (PAL_imb) : "memory")
 #define CLEAR_CACHE imb()
 
+/*FIXME probe broken in recent kernels, no access*/
+#define DEFINED_REAL_MAXPAGE (1UL<<18) /*FIXME brk probe broken*/
